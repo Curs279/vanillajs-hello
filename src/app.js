6f2e2303fead37c96eb7cf2bc18b7e80d5f99280
Curs279 <136276439+Curs279@users.noreply.github.com>
2023-09-14 05:16:39 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,39 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#excuse").innerHTML = generateExcuse();
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let pronoun = ["Mi"];
+  let subject = ["Hermano", "bebe", "perro", "padre", "hermana"];
+  let action = ["preparó", "cambió", "corrió hacia", "rompió"];
+  let object = ["el regalo", "el piso", "el baño", "la pelota", "la sorpresa"];
+  let where = [
+    "en el evento de revelación",
+    "en el parque",
+    "en el segundo piso",
+    "en el patio de la casa",
+    "en la academia Tempo"
+  ];
+
+  let proIndx = Math.floor(Math.random() * pronoun.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let objectIndex = Math.floor(Math.random() * object.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[proIndx] +
+    " " +
+    subject[subjectIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    object[objectIndex] +
+    " " +
+    where[whereIndex]
+  );
 };
